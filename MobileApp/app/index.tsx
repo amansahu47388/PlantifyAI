1774aac707cb74e8d22bc2336a30d53f1f84110c
Aman Sahu <amansahu47388@gmail.com>
2025-05-17 21:31:40 +0530

    mobile app setup

---
+++ MobileApp/app/index.tsx
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <Text style={styles.text}>Welcome to PlantifyAI</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  text: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#2c3e50',
+    textAlign: 'center',
+  }
+});
